96bce39a27563af5acccc6acf7c4f5a7b76c86c2
Matyas Tomolya <tomolya.matyas@gmail.com>
2017-04-22 18:27:15 +0100

    Correct backend url

--- app/js/sunnyExpressService.js
+++ app/js/sunnyExpressService.js
@@ -30,7 +30,7 @@ sunnyExpressApp.factory("SunnyExpress", function ($resource, $filter, $timeout, 
 
     var skyscannerAPI = "https://crossorigin.me/http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/:country/:currency/en-US/:depart/:arrive/:departureDate/:arriveDate";
 
-	var backendBaseUrl = "http://sunny-express-backend.herokuapp.com";
+	var backendBaseUrl = "http://sunny-express-backend.herokuapp.com/";
 
 	var cities = undefined;
 	var countries = undefined;
